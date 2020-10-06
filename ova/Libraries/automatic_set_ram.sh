5863f5f3fe02ee87a7a030b7ee1e9e6d69558799
Víctor Rebollo <victorrebollop@gmail.com>
2020-10-06 13:03:43 +0200

    Added tabulation

--- ova/Libraries/automatic_set_ram.sh
+++ ova/Libraries/automatic_set_ram.sh
@@ -3,7 +3,7 @@ ram_gb=$(free -m | awk '/^Mem:/{print $2}')
 ram=$(( ${ram_gb} / 2 ))
 
 if [ ${ram} -lt 1024 ]; then
-ram=$(( ${ram} / 2 ))
+    ram=$(( ${ram} / 2 ))
 fi
 
 sed -i "s/-Xms[0-9]\+[gm]/-Xms${ram}m/" "${etc_elastic}/jvm.options"
